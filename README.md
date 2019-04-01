6f7f446cea412b8ef400efcc48d37e0262237e91
Kathleen Ohde <kathleenohde@Kathleens-MacBook-Pro.local>
2019-04-01 11:40:46 -0500

    repsponsive updates morning 040118

--- README.md
+++
@@ -1,6 +0,0 @@
-# uiexperiments
-
-Clone project: `git clone https://username:password@github.com/ProKarma-Inc/uiexperiments.git` 
-
-In IDE of choice: go to the uiexperiments directory and `npm install`
-
--- controller/index.ctrl.js
+++ controller/index.ctrl.js
@@ -4,19 +4,19 @@ angular.module('app.core').controller('indexController', ['$scope', '$http', fun
     
     jQuery(document).ready(function($){
         
-        $('button.btn.btnResume').on('click', function(){
+        $('button.btn.btnResume').on('click', function() {
             var text = $('button.btn.btnResume').text();
             $('div.linked-in').toggleClass('show__info');
             $('button').text(
             text == "More Info" ? "Hide Info" : "More Info");
         });
 
-        $('li.dropper--color.color1').on('click', function(){
+        $('li.dropper--color.color1').on('click', function() {
             $('body').addClass('color1');
             $('body').removeClass('color2 color3 color4 color5 color6');
         });
         
-        $('li.dropper--color.color2').on('click', function(){
+        $('li.dropper--color.color2').on('click', function() {
             $('body').addClass('color2');
             $('body').removeClass('color1 color3 color4 color5 color6');
         });
@@ -26,28 +26,34 @@ angular.module('app.core').controller('indexController', ['$scope', '$http', fun
             $('body').removeClass('color1 color2 color4 color5 color6');
         });
 
-        $('li.dropper--color.color4').on('click', function(){
+        $('li.dropper--color.color4').on('click', function() {
             $('body').addClass('color4');
             $('body').removeClass('color1 color2 color3 color5 color6');
         });
 
-        $('li.dropper--color.color5').on('click', function(){
+        $('li.dropper--color.color5').on('click', function() {
             $('body').addClass('color5');
             $('body').removeClass('color1 color2 color3 color4 color6');
         });
 
-        $('li.dropper--color.color6').on('click', function(){
+        $('li.dropper--color.color6').on('click', function() {
             $('body').addClass('color6');
             $('body').removeClass('color1 color2 color3 color4 color5');
         });
 
-        $('div.hamburger-menu').on('click', function(){
+        $('div.hamburger-menu').on('click', function() {
            $('div.hamburger-menu').toggleClass('open__button');
            $('div.hamburger-menu').toggleClass('close__button');
            $('div.menu__big-circle').toggleClass('show__big-circle');
         });
+        
+        $('a.nav-links').on('click', function() {
+            $('div.hamburger-menu').toggleClass('close__button');
+            $('div.hamburger-menu').toggleClass('open__button');
+            $('div.menu__big-circle').toggleClass('show__big-circle');
+        })
 
-        $('div.dropper').on('click', function(){
+        $('div.dropper').on('click', function() {
             $('ul.dropper--colors').toggleClass('hide_dropper-colors');
          });
 
